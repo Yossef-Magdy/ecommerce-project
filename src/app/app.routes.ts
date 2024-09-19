6c98c2d1737e5c07431243ec9e2dcd51a08f567b
yossef <yossefmagdy78@gmail.com>
2024-09-19 03:21:30 +0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,52 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
+import { CollectionComponent } from './features/collection/collection.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './core/auth/pages/login/login.component';
+import { RegisterComponent } from './core/auth/pages/register/register.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home',
+    },
+    {
+        path: 'cart',
+        component: CartComponent,
+        title: 'Cart',
+    },
+    {
+        path: 'checkout',
+        component: CheckoutComponent,
+        title: 'Checkout',
+    },
+    {
+        path: 'collections',
+        component: CollectionComponent,
+        title: 'Collections',
+    },
+    {
+        path: 'contact',
+        component: ContactComponent,
+        title: 'Contact',
+    },
+    {
+        path: 'auth/login',
+        component: LoginComponent,
+        title: 'Login',
+    },
+    {
+        path: 'auth/register',
+        component: RegisterComponent,
+        title: 'Register',
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+        title: '404 Not Found',
+    }
+];
