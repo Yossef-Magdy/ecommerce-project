691ed7d0eb9b77b77cf6c87c13bc6d1b44982900
yossef <yossefmagdy78@gmail.com>
2024-09-19 23:46:38 +0300

    update login page

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { FormButtonComponent } from "../../components/form-button/form-button.component";
+import { LabelComponent } from "../../components/label/label.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormButtonComponent, RouterLink, LabelComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.min(5)]),
+  });
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+  get password() {
+    return this.loginForm.controls['password'];
+  }
 }
