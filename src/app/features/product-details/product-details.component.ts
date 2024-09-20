cfbac30592ce082658a0cce643e1430e4ea3a4a1
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-21 01:34:20 +0200

    create image gallery

--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { StarRatingComponent } from "../../shared/star-rating/star-rating.component";
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [StarRatingComponent, StarRatingComponent],
+  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
