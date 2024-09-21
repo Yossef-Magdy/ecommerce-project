ef1a84dd35c0d0121efe5dc46f4822ac4f805f1d
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-21 03:37:15 +0200

    update gallery swiper

---
+++ src/app/gallery-swiper/gallery-swiper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-swiper',
+  standalone: true,
+  imports: [],
+  templateUrl: './gallery-swiper.component.html',
+  styleUrl: './gallery-swiper.component.css'
+})
+export class GallerySwiperComponent {
+
+}
