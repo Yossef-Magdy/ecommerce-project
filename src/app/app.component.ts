8b6ba9d43b79d0fa199c589f6ec2795f0fe6d70a
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-21 02:21:34 +0200

    create gallery swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/layout/header/header.component";
 import { FooterComponent } from './core/layout/footer/footer.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,8 @@ import { FooterComponent } from './core/layout/footer/footer.component';
 })
 export class AppComponent {
   title = 'ecommerce';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { StarRatingComponent } from "../../shared/star-rating/star-rating.component";
 import { ButtonComponent } from '../../shared/button/button.component';
+import { GallerySwiperComponent } from "../../gallery-swiper/gallery-swiper.component";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent],
+  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
