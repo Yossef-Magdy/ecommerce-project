dee5609730e9a35303f9b800911a50848b14ca4a
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-21 04:33:51 +0200

    update product details functionality

--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { StarRatingComponent } from "../../shared/star-rating/star-rating.component";
 import { ButtonComponent } from '../../shared/button/button.component';
 import { GallerySwiperComponent } from "../gallery-swiper/gallery-swiper.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent],
+  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent, RouterLink],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
