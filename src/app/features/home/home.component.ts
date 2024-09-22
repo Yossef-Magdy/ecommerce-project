1032c6e7e6c9e6b077e7c7b4ac7303dbcd2bfe14
Hadeer Mohamed <mhadeer169@gmail.com>
2024-09-22 23:07:26 +0300

    home page style

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  currentIndex = 0;
+  categoryCards = [
+    {
+      title: 'Summer Collection',
+      image: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'Winter Collection',
+      image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'Kids Collection',
+      image: 'https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'Formal Collection',
+      image: 'https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    }
+  ];
+
+  clothsCards = [
+    {
+      title: 'Ripped Thick Strap Basic Top',
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'blue', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Ripped Fitted Half Sleeve Top',
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Swim Shorts',
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'pink', 'green', 'yellow'],
+      image: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Leggings',
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'blue', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    }
+  ]
+  nextCard() {
+    const maxIndex = this.categoryCards.length - 3;
+    if (this.currentIndex < maxIndex) {
+      this.currentIndex++;
+    }
+  }
+
+  prevCard() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+
+  getTransform() {
+    return `translateX(-${this.currentIndex * 33.33}%)`;
+  }
 }
