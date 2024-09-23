fb738d005a7014d21812f52e0f2b3735bf272a95
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-23 02:51:01 +0200

    update gallery thumbnail

--- src/app/features/gallery-swiper/gallery-swiper.component.ts
+++ src/app/features/gallery-swiper/gallery-swiper.component.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './gallery-swiper.component.css'
 })
 export class GallerySwiperComponent {
+  images = [
+    {"id": 1,
+      "src": "assets/1.jpg" 
+    },
+    {"id": 2,
+      "src": "assets/2.jpg" 
+    },
+    {"id": 3,
+      "src": "assets/3.jpg" 
+    },
+    {"id": 4,
+      "src": "assets/4.jpg" 
+    },
+    {"id": 5,
+      "src": "assets/5.jpg" 
+    },
+    {"id": 6,
+      "src": "assets/6.jpg" 
+    },
+    {"id": 1,
+      "src": "assets/1.jpg" 
+    },
+  ]
 
 }
