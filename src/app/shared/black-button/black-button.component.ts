0545f9e64d2d2bd248c07d1ddfaacaa69c6207ed
Hadeer Mohamed <mhadeer169@gmail.com>
2024-09-24 01:44:46 +0300

    black button

---
+++ src/app/shared/black-button/black-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-black-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './black-button.component.html',
+  styleUrl: './black-button.component.css'
+})
+export class BlackButtonComponent {
+  @Input() text: string = "";
+}
