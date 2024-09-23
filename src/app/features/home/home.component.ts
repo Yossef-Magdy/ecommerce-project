487094365af8a7f1265f2750c268ac64ec58faa1
Hadeer Mohamed <mhadeer169@gmail.com>
2024-09-23 22:04:34 +0300

    updating home page style

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { ArrowsComponent } from '../../shared/arrows/arrows.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ArrowsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  categoryCurrentIndex = 0; // تعديل اسم المتغير
+  clothsCurrentIndex = 0;
+  maxCategoryVisibleCards = 3;
+  maxClothsVisibleCards = 4;
 
-  currentIndex = 0;
   categoryCards = [
     {
       title: 'Summer Collection',
@@ -58,23 +62,30 @@ export class HomeComponent {
       newPrice: 'LE 150',
       colors: ['black', 'blue', 'white', 'gray'],
       image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Comfort T-shirt',
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'blue', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     }
-  ]
-  nextCard() {
-    const maxIndex = this.categoryCards.length - 3;
-    if (this.currentIndex < maxIndex) {
-      this.currentIndex++;
-    }
-  }
-
-  prevCard() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    }
-  }
-
+  ];
 
   getTransform() {
-    return `translateX(-${this.currentIndex * 33.33}%)`;
+    return `translateX(-${this.categoryCurrentIndex * 33.33}%)`;
   }
+
+  getClothsTransform() {
+    return `translateX(-${this.clothsCurrentIndex * (100 / this.maxClothsVisibleCards)}%)`;
+  }
+
+  onCategoryArrowClick(newIndex: number) {
+    this.categoryCurrentIndex = newIndex;
+  }
+
+  onClothsArrowClick(newIndex: number) {
+    this.clothsCurrentIndex = newIndex; // استخدام المتغير الجديد هنا
+  }
+
 }
