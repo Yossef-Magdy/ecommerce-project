4d265fb95579ef0365bc753e202aab74a8fc57c3
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-25 00:32:53 +0200

    add you may also like section

--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -4,11 +4,12 @@ import { ButtonComponent } from '../../shared/button/button.component';
 import { GallerySwiperComponent } from "../gallery-swiper/gallery-swiper.component";
 import { RouterLink } from '@angular/router';
 import { SizeChartComponent } from '../../core/auth/components/label/size-chart/size-chart.component';
+import { ProductCardComponent } from "../../shared/product-card/product-card.component";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent, RouterLink, SizeChartComponent],
+  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent, RouterLink, SizeChartComponent, ProductCardComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
