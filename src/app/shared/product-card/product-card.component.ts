fdcf917de35865e2c39815434dc7b3f566c7af82
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-25 01:44:14 +0200

    updates on product card

--- src/app/shared/product-card/product-card.component.ts
+++ src/app/shared/product-card/product-card.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { ArrowsComponent } from "../arrows/arrows.component";
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [ArrowsComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
+  clothsCurrentIndex = 0;
+  maxClothsVisibleCards = 6;
+
   clothsCards = [
     {
       title: 'Ripped Thick Strap Basic Top',
@@ -46,7 +50,31 @@ export class ProductCardComponent {
       oldPrice: 'LE 190',
       colors: ['black', 'blue', 'white', 'gray'],
       image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    }
+    },
+    {
+      title: 'Swim Shorts',
+      isSale: true,
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'pink', 'green', 'yellow'],
+      image: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Leggings',
+      isSale: true,
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'blue', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
   ]
+  
+  getClothsTransform() {
+    return `translateX(-${this.clothsCurrentIndex * (100 / this.maxClothsVisibleCards)}%)`;
+  }
+
+  onClothsArrowClick(newIndex: number) {
+    this.clothsCurrentIndex = newIndex; // استخدام المتغير الجديد هنا
+  }
 
 }
