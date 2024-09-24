f4830b6fe4e4b28d1f072a74d500231756a1bdd1
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-25 00:14:44 +0200

    separate product card for reusability

--- src/app/shared/product-card/product-card.component.ts
+++ src/app/shared/product-card/product-card.component.ts
@@ -8,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-
+  clothsCards = [
+    {
+      title: 'Ripped Thick Strap Basic Top',
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      isSale: true,
+      colors: ['black', 'blue', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Ripped Fitted Half Sleeve Top',
+      isSale: false,
+      price: 'LE 69',
+      colors: ['black', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Swim Shorts',
+      isSale: true,
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'pink', 'green', 'yellow'],
+      image: 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'Leggings',
+      isSale: true,
+      oldPrice: 'LE 190',
+      newPrice: 'LE 150',
+      colors: ['black', 'blue', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+    {
+      title: 'T-shirt',
+      isSale: 'true',
+      oldPrice: 'LE 190',
+      colors: ['black', 'blue', 'white', 'gray'],
+      image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    }
+  ]
+  
 }
