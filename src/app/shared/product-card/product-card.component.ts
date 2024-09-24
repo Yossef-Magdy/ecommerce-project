6fc70f41cf35ded6455f72d3a48d996be6a8bc3d
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-25 00:45:59 +0200

    add padding to cards

--- src/app/shared/product-card/product-card.component.ts
+++ src/app/shared/product-card/product-card.component.ts
@@ -48,5 +48,5 @@ export class ProductCardComponent {
       image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     }
   ]
-  
+
 }
