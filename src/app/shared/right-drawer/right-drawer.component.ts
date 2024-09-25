b7612a25964156886d42b65e5a65eb228255f661
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-25 22:30:11 +0200

    update menu wrapper

--- src/app/shared/right-drawer/right-drawer.component.ts
+++ src/app/shared/right-drawer/right-drawer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-right-drawer',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './right-drawer.component.css'
 })
 export class RightDrawerComponent {
+  @Input() title!: string;
 
 }
