e068502d899e477f078642a25eb6a165e2243ca6
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-25 05:30:34 +0200

    add zoom image feature

--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -5,15 +5,30 @@ import { GallerySwiperComponent } from "../gallery-swiper/gallery-swiper.compone
 import { RouterLink } from '@angular/router';
 import { SizeChartComponent } from '../../core/auth/components/label/size-chart/size-chart.component';
 import { ProductCardComponent } from "../../shared/product-card/product-card.component";
+import { ZoomComponent } from "../zoom/zoom.component";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent, RouterLink, SizeChartComponent, ProductCardComponent],
+  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent, RouterLink, SizeChartComponent, ProductCardComponent, ZoomComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+
+  product = {
+    "slug": "slug",
+    "name": "name",
+    "description": "desc",
+    "images": [
+      "assets/1.jpg",
+      "assets/2.jpg",
+      "assets/3.jpg",
+      "assets/4.jpg",
+      "assets/5.jpg",
+      "assets/6.jpg"
+    ],
+  };
   colors = ["red", "black", "grey", "beige"];
   sizes = ["S", "M", "L", "XL"];
 
