501d96b019b66df7d353b632db8508ca6e8f6a87
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-25 14:26:04 +0200

    add menu wrapper

--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -6,11 +6,12 @@ import { RouterLink } from '@angular/router';
 import { SizeChartComponent } from '../../core/auth/components/label/size-chart/size-chart.component';
 import { ProductCardComponent } from "../../shared/product-card/product-card.component";
 import { ZoomComponent } from "../zoom/zoom.component";
+import { RightDrawerComponent } from "../../shared/right-drawer/right-drawer.component";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent, RouterLink, SizeChartComponent, ProductCardComponent, ZoomComponent],
+  imports: [StarRatingComponent, StarRatingComponent, ButtonComponent, GallerySwiperComponent, GallerySwiperComponent, RouterLink, SizeChartComponent, ProductCardComponent, ZoomComponent, RightDrawerComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
---
+++ src/app/shared/right-drawer/right-drawer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-drawer',
+  standalone: true,
+  imports: [],
+  templateUrl: './right-drawer.component.html',
+  styleUrl: './right-drawer.component.css'
+})
+export class RightDrawerComponent {
+
+}
