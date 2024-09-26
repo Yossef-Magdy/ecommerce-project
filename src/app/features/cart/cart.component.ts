a90e286500a84d8758129a5b88c7387da9b09ada
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-26 05:49:29 +0200

    implement empty cart

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  data = [];
+  totalPrice: number = 0;
 
 }
