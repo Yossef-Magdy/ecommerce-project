dc741ec86e333569caff57e9f531c1087274a12e
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-27 06:55:53 +0200

    change location of component

---
+++ src/app/features/product-details/zoom/zoom.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { GallerySwiperComponent } from '../gallery-swiper/gallery-swiper.component';
+
+@Component({
+  selector: 'app-zoom',
+  standalone: true,
+  imports: [GallerySwiperComponent],
+  templateUrl: './zoom.component.html',
+  styleUrl: './zoom.component.css',
+})
+export class ZoomComponent {
+  @Input() images!: any;
+}
