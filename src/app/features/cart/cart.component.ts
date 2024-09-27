48891e23eb05dea0e57de0895f6a3de6393bcb81
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-27 06:53:11 +0200

    add subtotal section

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartItemComponent } from "./cart-item/cart-item.component";
+import { BlackButtonComponent } from "../../shared/black-button/black-button.component";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [RouterLink, CartItemComponent],
+  imports: [RouterLink, CartItemComponent, BlackButtonComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
