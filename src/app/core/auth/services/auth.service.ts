91b96a3682c1b0c7cec5d7b98c2da2ad8fdd5c09
yossef <yossefmagdy78@gmail.com>
2024-10-02 00:09:51 +0300

    implements auth service

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,9 +1,73 @@
 import { Injectable } from '@angular/core';
-
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpStatusCode,
+} from '@angular/common/http';
+import { catchError, concatMap, Observable, of, tap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private userData: any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(userData: any) {
+    return this.http.post('/login', userData).pipe(
+      tap((response: any) => {
+        this.savaToken(response);
+        this.goHome();
+      })
+    );
+  }
+
+  register(userData: any) {
+    return this.http.post('/register', userData).pipe(
+      tap((response: any) => {
+        this.savaToken(response);
+        this.goHome();
+      })
+    );
+  }
+  
+  logout() {
+    this.removeToken();
+    this.setUserData(null);
+  }
+  
+  getUser() {
+    return this.http.get('/user');
+  }
+
+  getUserData() {
+    return this.userData;
+  }
+  
+  setUserData(userData: any) {
+    this.userData = userData;
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+  
+  tokenExists() {
+    const token = this.getToken();
+    return token == null ? false : true;
+  }
+
+  private removeToken() {
+    localStorage.removeItem('token');
+  }
+
+  private goHome() {
+    this.router.navigate(['/']);
+  }
+
+  private savaToken(response: any) {
+    const authToken = `${response.token_type} ${response.access_token}`;
+    localStorage.setItem('token', authToken);
+  }
 }
