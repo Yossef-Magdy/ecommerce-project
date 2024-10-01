6f9169080db1baa071c9ed03563a0d9f86129c87
yossef <yossefmagdy78@gmail.com>
2024-10-02 00:17:35 +0300

    update header

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, CSP_NONCE, DoCheck, EventEmitter, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartItem, CartService } from '../../../services/cart.service';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,17 +10,58 @@ import { CartItem, CartService } from '../../../services/cart.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements DoCheck {
   cartItems: CartItem[] = [];
   totalQuantity: number = 0;
+  curToken: string | null  = null;
+  validLogin: boolean = false;
+  userData: any;
 
-  constructor(private cartService:CartService){}
+  constructor(private cartService:CartService, private authService: AuthService){}
 
   ngOnInit(){
+    this.curToken = this.authService.getToken();
+    this.isLoggedIn(this.curToken);
     this.cartService.getItems().subscribe((items) => {
       this.cartItems = items;
       this.calculateTotalQuantity();
-    })
+    });
+  }
+
+  ngDoCheck() {
+    if (this.curToken != this.authService.getToken()) {
+      this.curToken = this.authService.getToken();
+      this.isLoggedIn(this.curToken);
+    }
+  }
+
+  isLoggedIn(token: string | null) {
+    if (!this.authService.tokenExists()) {
+      this.validLogin = false;
+      return;
+    }
+    this.authService.getUser().subscribe({
+      next: (response) => {
+        this.userData = response;
+        this.authService.setUserData(response);
+        this.validLogin = true;
+      },
+      error: (error) => {
+        this.logout();
+        this.validLogin = false;
+      }
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  hasRolesOrPermissions() {
+    if (this.userData == null) {
+      return false;
+    }
+    return this.userData.roles.length || this.userData.permissions.length;
   }
 
   calculateTotalQuantity(): void{
