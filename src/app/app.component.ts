c4e56cc86e03284665f7b20580676401d68a6a8c
yossef <yossefmagdy78@gmail.com>
2024-10-02 16:27:54 +0300

    prepare dashboard stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/layout/header/header.component";
 import { FooterComponent } from './core/layout/footer/footer.component';
 import { initFlowbite } from 'flowbite';
+import { Router } from '@angular/router';
+import { ControlHeaderComponent } from "./core/layout/control-header/control-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ControlHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecommerce';
 
+  constructor (private router: Router) {}
+
   ngOnInit(): void {
     initFlowbite();
+  }
+
+  inDashboard() {
+    return this.router.url.startsWith('/dashboard');
   }
 }
