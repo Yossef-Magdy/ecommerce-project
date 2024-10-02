1fc15dfd67bdd7af2e488a01d6456d6af15aa315
Esraa_GamalH <esraa9gamal@gmail.com>
2024-10-02 05:53:14 +0200

    update gallery to be responsive

--- src/app/features/product-details/gallery-swiper/gallery-swiper.component.ts
+++ src/app/features/product-details/gallery-swiper/gallery-swiper.component.ts
@@ -1,37 +1,48 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-
+import { Component, HostListener, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-gallery-swiper',
   standalone: true,
-  imports: [],
   templateUrl: './gallery-swiper.component.html',
-  styleUrl: './gallery-swiper.component.css',
+  styleUrls: ['./gallery-swiper.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GallerySwiperComponent {
+  // Default direction is set to vertical
+  direction: 'horizontal' | 'vertical' = 'vertical';
+  swiperStyle: any = {};  // Store styles dynamically
+
   images = [
-    {"id": 1,
-      "src": "assets/1.jpg" 
-    },
-    {"id": 2,
-      "src": "assets/2.jpg" 
-    },
-    {"id": 3,
-      "src": "assets/3.jpg" 
-    },
-    {"id": 4,
-      "src": "assets/4.jpg" 
-    },
-    {"id": 5,
-      "src": "assets/5.jpg" 
-    },
-    {"id": 1,
-      "src": "assets/1.jpg" 
-    },
-  ]
+    { id: 1, src: "assets/1.jpg" },
+    { id: 2, src: "assets/2.jpg" },
+    { id: 3, src: "assets/3.jpg" },
+    { id: 4, src: "assets/4.jpg" },
+    { id: 5, src: "assets/5.jpg" },
+    { id: 6, src: "assets/1.jpg" },
+  ];
 
+  constructor() {
+    // Set initial direction and styles based on the screen size
+    this.updateDirectionBasedOnScreenSize();
+  }
 
+  // Listen for window resize events to dynamically change direction and styles
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.updateDirectionBasedOnScreenSize();
+  }
+
+  // Update Swiper direction and styles based on screen width
+  updateDirectionBasedOnScreenSize() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 768) {
+      // Set vertical view for large screens
+      this.direction = 'vertical';
+      this.swiperStyle = { width: '200px', height: '500px' };
+    } else {
+      // Set horizontal view for smaller screens
+      this.direction = 'horizontal';
+      this.swiperStyle = { width: '400px', height: '100px', position: 'static'};
+    }
+  }
 }
