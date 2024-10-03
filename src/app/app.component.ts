3ab27afb7d68fa9dd0b2d34838c68fd037819993
yossef <yossefmagdy78@gmail.com>
2024-10-03 13:31:46 +0300

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/layout/header/header.component";
 import { FooterComponent } from './core/layout/footer/footer.component';
+import { SidebarComponent } from './core/layout/sidebar/sidebar.component';
 import { initFlowbite } from 'flowbite';
 import { Router } from '@angular/router';
-import { ControlHeaderComponent } from "./core/layout/control-header/control-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ControlHeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
