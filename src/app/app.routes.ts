7fcb29131c9ae4edfce014aa8d5354275cc2f729
yossef <yossefmagdy78@gmail.com>
2024-10-05 01:03:45 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,53 +13,54 @@ import { ProductDetailsComponent } from './features/product-details/product-deta
 import { ForbiddenComponent } from './features/forbidden/forbidden.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 
+const appName = 'Temp';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-        title: 'Home',
+        title: `Home - ${appName}`,
     },
     {
         path: 'cart',
         component: CartComponent,
-        title: 'Cart',
+        title: `Cart - ${appName}`,
     },
     {
         path: 'checkout',
         component: CheckoutComponent,
-        title: 'Checkout',
+        title: `Checkout - ${appName}`,
     },
     {
         path: 'collections',
         component: CollectionComponent,
-        title: 'Collections',
+        title: `Collections - ${appName}`,
     },
     {
         path: 'contact',
         component: ContactComponent,
-        title: 'Contact',
+        title: `Contact - ${appName}`,
     },
     {
         path: 'auth/login',
         component: LoginComponent,
-        title: 'Login',
+        title: `Login - ${appName}`,
     },
     {
         path: 'auth/register',
         component: RegisterComponent,
-        title: 'Register',
+        title: `Register - ${appName}`,
     },
     {
 
         path: 'profile',
         component: ProfileComponent,
-        title: 'Profile',
+        title: `Profile - ${appName}`,
     },
     {
         path: 'products/:id',
         component: ProductDetailsComponent,
-        title: 'products',
+        title: `Products - ${appName}`,
     },
     {
         path: 'dashboard',
@@ -67,19 +68,18 @@ export const routes: Routes = [
             {
                 path: '',
                 component: DashboardComponent,
-                title: 'dashboard',
+                title: `Dashboard - ${appName}`,
             },
-
         ]
     },
     {
         path: 'forbidden',
         component: ForbiddenComponent,
-        title: 'forbidden',
+        title: `Forbidden - ${appName}`,
     },
     {
         path: '**',
         component: NotFoundComponent,
-        title: '404 Not Found',
+        title: `404 Not Found - ${appName}`,
     }
 ];
