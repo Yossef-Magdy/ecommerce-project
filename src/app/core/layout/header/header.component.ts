f279b1cd41e5e5cb542fe61c58fb5cb0b74091d9
yossef <yossefmagdy78@gmail.com>
2024-10-05 17:52:07 +0300

    update header

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,8 +1,7 @@
-import { Component, CSP_NONCE, DoCheck, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartItem, CartService } from '../../../services/cart.service';
 import { AuthService } from '../../auth/services/auth.service';
-import { HttpStatusCode } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -15,7 +14,7 @@ export class HeaderComponent {
   cartItems: CartItem[] = [];
   totalQuantity: number = 0;
   validLogin: boolean = false;
-  hasRolesOrPermissions?: boolean = false;
+  hasRolesOrPermissions: boolean = false;
 
   constructor(private cartService:CartService, private authService: AuthService){}
 
@@ -26,10 +25,9 @@ export class HeaderComponent {
     });
     this.authService.checkUser().subscribe((isValid: boolean) => {
       this.validLogin = isValid;
-      if (this.validLogin) {
-        const userData = this.authService.getUserData();
-        this.hasRolesOrPermissions = userData.roles.length || userData.permissions.length;
-      }
+    });
+    this.authService.hasRolesOrPermissions.subscribe((value: boolean) => {
+      this.hasRolesOrPermissions = value;
     });
   }
 
