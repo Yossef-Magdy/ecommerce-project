bc6fa49a37abccd13eb023e7f1b5d4497b1dd3f4
Esraa_GamalH <esraa9gamal@gmail.com>
2024-10-07 07:34:03 +0200

    implement empty reviews

--- src/app/features/product-details/product-reviews/product-reviews.component.ts
+++ src/app/features/product-details/product-reviews/product-reviews.component.ts
@@ -1,18 +1,18 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductReviewsServiceService } from './product-reviews-service.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { StarRatingComponent } from "../../../shared/star-rating/star-rating.component";
 
 @Component({
   selector: 'app-product-reviews',
   standalone: true,
-  imports: [NgFor, StarRatingComponent],
+  imports: [NgFor, StarRatingComponent, RouterLink],
   templateUrl: './product-reviews.component.html',
   styleUrl: './product-reviews.component.css'
 })
 export class ProductReviewsComponent {
-  reviews !: any[];
+  reviews : any[] = [];
   constructor(private productReviewsService:ProductReviewsServiceService, private activatedRoute: ActivatedRoute){}
 
   ngOnInit(){
