91318e24ed293f816956178a15f0ace0de5b4060
yossef <yossefmagdy78@gmail.com>
2024-10-11 14:18:48 +0300

    implement add coupon

--- src/app/features/dashboard/pages/add-coupon/add-coupon.component.ts
+++ src/app/features/dashboard/pages/add-coupon/add-coupon.component.ts
@@ -1,12 +1,93 @@
 import { Component } from '@angular/core';
-
+import { LabelComponent } from "../../../../core/auth/components/label/label.component";
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { BlackButtonComponent } from "../../../../shared/black-button/black-button.component";
+import { CouponService } from '../../services/coupon.service';
+import { fromEvent } from 'rxjs';
 @Component({
   selector: 'app-add-coupon',
   standalone: true,
-  imports: [],
+  imports: [LabelComponent, ReactiveFormsModule, BlackButtonComponent],
   templateUrl: './add-coupon.component.html',
   styleUrl: './add-coupon.component.css'
 })
 export class AddCouponComponent {
 
+  couponForm: FormGroup;
+
+  constructor(private couponSerivce: CouponService) {
+    this.couponForm = new FormGroup({
+      couponCode: new FormControl('', [Validators.required]),
+      usesCount: new FormControl(50, [Validators.min(50)]),
+      discountType: new FormControl('fixed'),
+      discountValue: new FormControl(0, [Validators.min(0), this.invalidPercent()]),
+      expiryDate: new FormControl(new Date().toISOString().substring(0, 10), [this.pastDate()]),
+    });
+    this.discountType?.valueChanges.subscribe(() => {
+      this.discountValue.updateValueAndValidity();
+    });
+  }
+
+  message?: string;
+  isErrorMessage?: boolean;
+
+  get couponCode() {
+    return this.couponForm.controls['couponCode'];
+  }
+
+  get usesCount() {
+    return this.couponForm.controls['usesCount'];
+  }
+
+  get expiryDate() {
+    return this.couponForm.controls['expiryDate'];
+  }
+
+  get discountType() {
+    if (this.couponForm) {
+      return this.couponForm.controls['discountType'];
+    }
+    return null;
+  }
+
+  get discountValue() {
+    return this.couponForm.controls['discountValue'];
+  }
+
+  pastDate(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const today = new Date()
+      const date = new Date(control.value);
+      if (date < today ) {
+        return { pastDate: true };
+      }
+      return null;
+    }
+  }
+
+  invalidPercent(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (this.discountType?.value != 'fixed' && control.value > 100) {
+        return { invalidPercent: true };
+      }
+      return null;
+    }
+  }
+
+  submit() {
+    this.couponForm.markAllAsTouched();
+    if (this.couponForm.invalid) {
+      return;
+    }
+    this.couponSerivce.addCoupon(this.couponForm.value).subscribe((response: any) => {
+      if (response.message) {
+        this.isErrorMessage = false;
+        this.message = response.message;
+      } else {
+        this.isErrorMessage = true;
+        const key = Object.keys(response)[0];
+        this.message = response[key][0];
+      }
+    });
+  }
 }
