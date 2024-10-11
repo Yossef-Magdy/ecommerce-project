2dc39781edad44b297717bf3b3716a4530e9e1b8
yossef <yossefmagdy78@gmail.com>
2024-10-11 21:58:37 +0300

    update coupon service

--- src/app/features/dashboard/services/coupon.service.ts
+++ src/app/features/dashboard/services/coupon.service.ts
@@ -6,6 +6,8 @@ import { catchError, of } from 'rxjs';
 })
 export class CouponService {
 
+  private baseURL = '/control/coupons';
+
   constructor(private http: HttpClient) { }
 
   addCoupon(coupon: any) {
@@ -24,13 +26,40 @@ export class CouponService {
     return this.http.get('/control/coupons');
   }
 
+  updateCoupon(coupon: any, couponId: number) {
+    coupon = this.prepareCoupon(coupon);
+    return this.http.put(`${this.baseURL}/${couponId}`, coupon).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when updating the coupon");
+      })
+    );
+  }
+
+  deleteCoupon(couponId: number) {
+    return this.http.delete(`${this.baseURL}/${couponId}`).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when deleting the coupon");
+      })
+    );
+  }
+
   private prepareCoupon(coupon: any) {
-    return {
+    const new_coupon: any = {
       coupon_code: coupon.couponCode,
       uses_count: coupon.usesCount,
       discount_type: coupon.discountType, 
       discount_value: coupon.discountValue,
       expiry_date: coupon.expiryDate,
     };
+    if (coupon.id) {
+      new_coupon.id = coupon.id;
+    }
+    return new_coupon;
   }
 }
