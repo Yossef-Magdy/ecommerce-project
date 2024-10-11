b3f0afe83ea743cd6b6e056c1bcfd662db777d30
yossef <yossefmagdy78@gmail.com>
2024-10-11 14:20:51 +0300

    implement category service

--- src/app/features/dashboard/services/category.service.ts
+++ src/app/features/dashboard/services/category.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  addCategory(data: any) {
+    return this.http.post('/control/categories', data).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when adding the category");
+      })
+    );
+  }
+
+  addSubcategory(data: any) {
+    data = {
+      name: data.name,
+      category_id: data.categoryName,
+    };
+    return this.http.post('/control/subcategories', data).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when adding the subcategory");
+      })
+    );
+  }
+
+  getCategories() {
+    return this.http.get('/categories');
+  }
+
+  getSubcategories() {
+    return this.http.get('/subcategories');
+  }
+
 }
