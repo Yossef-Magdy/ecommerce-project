dbc970f867633585e3d79857b490fe6e0b36a276
yossef <yossefmagdy78@gmail.com>
2024-10-11 14:21:17 +0300

    implement coupon service

--- src/app/features/dashboard/services/coupon.service.ts
+++ src/app/features/dashboard/services/coupon.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpStatusCode } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CouponService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addCoupon(coupon: any) {
+    coupon = this.prepareCoupon(coupon); 
+    return this.http.post('/control/coupons', coupon).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when adding the coupon");
+      })
+    );
+  }
+
+  getCoupons() {
+    return this.http.get('/control/coupons');
+  }
+
+  private prepareCoupon(coupon: any) {
+    return {
+      coupon_code: coupon.couponCode,
+      uses_count: coupon.usesCount,
+      discount_type: coupon.discountType, 
+      discount_value: coupon.discountValue,
+      expiry_date: coupon.expiryDate,
+    };
+  }
 }
