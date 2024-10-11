a25aa618c6b50bdc06a7f8b8f52ce8105b8d74f1
yossef <yossefmagdy78@gmail.com>
2024-10-11 14:19:09 +0300

    implement add governorates

---
+++ src/app/features/dashboard/pages/add-governorate/add-governorate.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { LabelComponent } from "../../../../core/auth/components/label/label.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BlackButtonComponent } from "../../../../shared/black-button/black-button.component";
+import { GovernorateService } from '../../services/governorate.service';
+
+@Component({
+  selector: 'app-add-governorate',
+  standalone: true,
+  imports: [LabelComponent, ReactiveFormsModule, BlackButtonComponent],
+  templateUrl: './add-governorate.component.html',
+  styleUrl: './add-governorate.component.css'
+})
+export class AddGovernorateComponent {
+  governorateForm: FormGroup;
+
+  constructor(private governorateService: GovernorateService) {
+    this.governorateForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+    });
+  }
+
+  message?: string;
+  isErrorMessage?: boolean;
+
+  get name() {
+    return this.governorateForm.controls['name'];
+  }
+
+  submit() {
+    if (this.governorateForm.invalid) {
+      return;
+    }
+    this.governorateService.addGovernorate(this.governorateForm.value).subscribe((response: any) => {
+      if (response.message) {
+        this.isErrorMessage = false;
+        this.message = response.message;
+      } else {
+        this.isErrorMessage = true;
+        const key = Object.keys(response)[0];
+        this.message = response[key][0];
+      }
+    });
+  }
+}
