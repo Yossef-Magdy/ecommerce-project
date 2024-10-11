22e47e528d536ac826c2da10f718e939f89cf51d
yossef <yossefmagdy78@gmail.com>
2024-10-11 14:21:49 +0300

    update user service

--- src/app/features/dashboard/services/user.service.ts
+++ src/app/features/dashboard/services/user.service.ts
@@ -7,13 +7,14 @@ import { catchError, map, of } from 'rxjs';
 })
 export class UserService {
 
-  private baseURL = '/control/users';
+  private userURL = '/control/users';
+  private roleURL = '/control/roles';
 
   constructor(private http: HttpClient) { }
 
   addUser(userData: any) {
     userData = this.prepareUserData(userData);
-    return this.http.post(this.baseURL, userData).pipe(
+    return this.http.post(this.userURL, userData).pipe(
       catchError((error) => {
         if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
           return of(error.error.errors);
@@ -24,7 +25,7 @@ export class UserService {
   }
 
   addRole(data: any) {
-    return this.http.post('/control/roles', data).pipe(
+    return this.http.post(this.roleURL, data).pipe(
       catchError((error) => {
         if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
           return of(error.error.errors);
@@ -35,12 +36,30 @@ export class UserService {
   }
 
   getUsers() {
-    return this.http.get(this.baseURL);
+    return this.http.get(this.userURL);
   }
 
-  updateUser(userData: any , userId: number) {
-    userData = this.prepareUserData(userData);
-    return this.http.put(`${this.baseURL}/${userId}`, userData);
+  updateUser(userData: any) {
+    const userId: number = userData.id;
+    return this.http.put(`${this.userURL}/${userId}`, userData).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when updating the user");
+      })
+    );
+  }
+
+  deleteUser(userId: number) {
+    return this.http.delete(`${this.userURL}/${userId}`).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when deleting the user");
+      })
+    );  
   }
 
   private prepareUserData(userData: any) {
@@ -55,12 +74,42 @@ export class UserService {
   }
 
   getRoles() {
-    return this.http.get('/control/roles').pipe(
+    return this.http.get(this.roleURL).pipe(
+      map((roles: any) => {
+        roles.data.map((role: any) => {
+          role.permissions.map((permission: any) => {
+            permission.name = permission.name.replace('-', ' ');
+          });
+        });
+        return roles;
+      }),
       catchError ((error) => {
         console.log('an error occurred when getting roles')
         return of ([])
       })
     )
+  }
+
+  updateRole(data: any, roleId: number) {
+    return this.http.put(`${this.roleURL}/${roleId}`, data).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when updating the role");
+      })
+    );
+  }
+
+  deleteRole(roleId: number) {
+    return this.http.delete(`${this.roleURL}/${roleId}`).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when deleting the role");
+      })
+    );  
   }
 
   getPermissions() {
