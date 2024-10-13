c53a7b8920367af2fd2bbcac9502d74876cfc3c1
yossef <yossefmagdy78@gmail.com>
2024-10-13 16:24:52 +0300

    add toast

---
+++ src/app/core/services/toast.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor() { }
+  
+  private isVisibleSubject = new BehaviorSubject<boolean>(false);
+  private messageSubject = new BehaviorSubject<string>('test text');
+  private typeSubject = new BehaviorSubject<'success' | 'error'>('success');
+
+  isVisible = this.isVisibleSubject.asObservable();
+  message = this.messageSubject.asObservable();
+  type = this.typeSubject.asObservable();
+
+  setMessage(message: string) {
+    this.messageSubject.next(message);
+  }
+
+  setType(type: 'success' | 'error') {
+    this.typeSubject.next(type);
+  }
+
+  showToast() {
+    this.isVisibleSubject.next(true);
+    setTimeout(() => {
+      this.hideToast();
+    }, 3000);
+  }
+
+  hideToast() {
+    this.isVisibleSubject.next(false);
+  }
+
+}
