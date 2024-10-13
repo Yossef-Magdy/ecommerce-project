5dbb37f72d9c41126c76321e9e83bbb1e3d00305
yossef <yossefmagdy78@gmail.com>
2024-10-13 13:16:13 +0300

    update product service

--- src/app/features/dashboard/services/product.service.ts
+++ src/app/features/dashboard/services/product.service.ts
@@ -21,4 +21,30 @@ export class ProductService {
       })
     );
   }
+
+  getProducts() {
+    return this.http.get(this.baseURL);
+  }
+
+  updateProduct(data: any, productId: number) {
+    return this.http.put(`${this.baseURL}/${productId}`, data).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when updating the product");
+      })
+    );
+  }
+
+  deleteProduct(productId: number) {
+    return this.http.delete(`${this.baseURL}/${productId}`).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("an error occured when deleting the product");
+      })
+    );
+  }
 }
