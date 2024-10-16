00268a445b68e979b958da5f5e0698c1046dfcbe
yossef <yossefmagdy78@gmail.com>
2024-10-16 05:14:50 +0300

    add product details service

---
+++ src/app/features/dashboard/services/product-details.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastService } from '../../../core/services/toast.service';
+import { catchError, map, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailsService {
+  private baseURL = '/control/product-details';
+
+  constructor(private http: HttpClient, private toastService: ToastService) { }
+
+  addProductDetails(data: any) {
+    return this.http.post(this.baseURL, data).pipe(
+      tap((result: any) => {
+        this.toastService.showToast(result.message, 'success');
+      }),
+      catchError((error: any) => of(false))
+    );
+  }
+
+  updateProductDetail(data: any, productDetailId: number) {
+    return this.http.put(`${this.baseURL}/${productDetailId}`, data).pipe(
+      tap((result: any) => {
+        this.toastService.showToast(result.message, 'success');
+      }), catchError((error: any) => {
+        return of(false);
+      })
+    );
+  }
+
+  deleteProductDetail(productDetailId: number) {
+    return this.http.delete(`${this.baseURL}/${productDetailId}`).pipe(
+      tap((result: any) => {
+        this.toastService.showToast(result.message, 'success');
+      }),
+      map((result: any) => true),
+      catchError((error: any) => of(false))
+    );
+  }
+}
