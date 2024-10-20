c9cd628741e646c16d26afdf7f45c6dfbc47b16c
Esraa_GamalH <esraa9gamal@gmail.com>
2024-10-20 04:03:14 +0200

    add get method to coupon service

--- src/app/features/dashboard/services/coupon.service.ts
+++ src/app/features/dashboard/services/coupon.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpStatusCode } from '@angular/common/http';
-import { catchError, map, of, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { ToastService } from '../../../core/services/toast.service';
 @Injectable({
   providedIn: 'root'
@@ -26,6 +26,17 @@ export class CouponService {
       catchError ((error) => {
         this.toastService.showToast('an error occurred when getting coupons', 'error');
         return of ([])
+      })
+    );
+  }
+
+  getCouponByCode(couponCode: string):Observable<any>{
+    return this.http.get(`${this.baseURL}/${couponCode}`).pipe(
+      catchError((error) => {
+        if (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.UnprocessableEntity) {
+          return of(error.error.errors);
+        }
+        return of("coupon not found");
       })
     );
   }
