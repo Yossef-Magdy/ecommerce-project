a22dbffb9b71d39b35eb5c7432e233a9e0aaa56f
Esraa_GamalH <esraa9gamal@gmail.com>
2024-10-27 02:36:11 +0200

    update order summary page

--- src/app/features/order-success/order-success.component.ts
+++ src/app/features/order-success/order-success.component.ts
@@ -2,79 +2,52 @@ import { Component } from '@angular/core';
 import { OrderService } from './service/order.service';
 import { MyCurrencyPipe } from '../../pipes/my-currency.pipe';
 import { NgIf } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../profile/user.service';
+import { GovernorateService } from '../profile/governorate.service';
 
 @Component({
   selector: 'app-order-success',
   standalone: true,
-  imports: [MyCurrencyPipe, NgIf],
+  imports: [MyCurrencyPipe, NgIf, RouterLink],
   templateUrl: './order-success.component.html',
   styleUrl: './order-success.component.css'
 })
 export class OrderSuccessComponent {
-  order: any = {
-    "message": "Order created successfully",
-    "data": {
-        "id": 91,
-        "paid_amount": 0,
-        "outstanding_amount": 207,
-        "total_price": 207,
-        "shipping": {
-            "status": "Pending",
-            "shipping_detail": {
-                "id": 17,
-                "city": "Hawamdia",
-                "address": "11st, Hawamdia, Giza, Egypt",
-                "apartment": "5",
-                "postal_code": "12916",
-                "phone_number": "01127152192",
-                "is_default": true,
-                "governorate": "giza"
-            }
-        },
-        "payment": {
-            "method": "cod",
-            "status": "Incomplete"
-        },
-        "coupon": null,
-        "items": [
-            {
-                "item_id": 101,
-                "size": "2-3",
-                "color": "brown",
-                "material": "quick-drying fabric",
-                "quantity": 1,
-                "price": 220,
-                "total_price": 187,
-                "discount": 33,
-                "product": {
-                    "product_detail_id": 34,
-                    "product_id": 14,
-                    "name": "Kids Swim Shorts",
-                    "description": "These kids swim shorts offer comfort and style for your little ones. Made from high-quality materials, they provide a secure fit and quick-drying fabric for hours of fun in the water. The perfect choice for any active child! The model is 1.88 m wearing size L",
-                    "image": "http://127.0.0.1:8000/cover/pjcZISFzeRkXMHJkKBKLe5lcZpok6SiWi35YXcRl.webp",
-                    "price": 220
-                }
-            }
-        ],
-        "created_at": "2024-10-26T19:28:33.000000Z",
-        "updated_at": "2024-10-26T19:28:33.000000Z"
-    },
-    "success": true,
-    "charge": null
-}
+  order!: any;
+  orderSummary!: any;
+  orderOverall!: any;
+  shipping!: any;
+  shippingFee: number = 0;
+  payment!: any;
+  user !: any;
 
-orderSummary: any = this.order.data.items;
-orderOverall: any = this.order.data;
-shipping: any = this.order.data.shipping.shipping_detail;
-payment: any = this.order.data.payment;
-
-  constructor(private orderService:OrderService){}
+  constructor(private orderService:OrderService, private userService: UserService, private router: Router, private governerateService: GovernorateService){}
 
   ngOnInit() {
     this.orderService.getOrderSummary().pipe().subscribe((order)=>{
-      this.order = order.data;
-      console.log(order);
+      console.log("order", !order);
       
+      if (!Object.keys(order).length) this.router.navigate(['/']);
+      else{
+        this.order = order.data;
+        this.orderSummary = order.data.items;
+        this.orderOverall = order.data;
+        this.shipping = order.data.shipping.shipping_detail; 
+        this.payment = order.data.payment;
+        this.getShippingCustomFee(order.data.shipping.shipping_detail.governorate);
+      }
+    });
+
+    this.userService.getUserData().subscribe((res: any) => {
+      this.user = res;
+    });
+  }
+
+  getShippingCustomFee(governorate: string): any{
+    this.governerateService.getGovernorates().subscribe(govs => {
+      const fee = govs.data.filter((g: any) => g.name === governorate)[0].fee;
+      this.shippingFee = fee;
     })
   }
 
